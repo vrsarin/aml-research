56aa8ead4ad35a35d10ceb4e16a21619f3681ae2
Vivek Sarin <vivek@sarins.info>
2023-11-20 07:40:43 -0500

    Basic service changes

--- svc/external/info.sarins.services.knowledge/Controllers/NodesController.cs
+++ svc/external/info.sarins.services.knowledge/Controllers/NodesController.cs
@@ -1,26 +1,83 @@
 ﻿using info.sarins.services.knowledge.Data;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
 
 namespace info.sarins.services.knowledge.Controllers
 {
-    [Route("case-files/{identifier}/knowledge/entities")]
+    [Route("case-files/{identifier}/knowledge/graph")]
     [ApiController]
     public class NodesController : ControllerBase
     {
-        [HttpGet("{type}")]
-        [SwaggerOperation(Summary = "Get all entities",
-           Description = "Requires admin privileges",
-           OperationId = "GetAllCases",
-           Tags = new[] { "Knowledge" }
-           )]       
-        public async Task<IActionResult> GetNodes(long identifier, string type)
+        private readonly ILogger<NodesController> logger;
+        private readonly INeo4JDB neo4JDB;
+
+        public NodesController(ILogger<NodesController> logger, INeo4JDB neo4JDB)
         {
-            using var neo4Jdb = new Neo4JDB();
-            var labels = await neo4Jdb.GetNodesWithProperties(type);
+            this.logger = logger;
+            this.neo4JDB = neo4JDB;
+        }
+
+
+        [HttpGet]
+        [SwaggerOperation(Summary = "Get all nodes",
+          Description = "Requires admin privileges",
+          OperationId = "GetAllCases",
+          Tags = new[] { "Nodes" }
+          )]
+        public async Task<IActionResult> GetAllNodes(long identifier)
+        {
+            var labels = await neo4JDB.GetNodesWithProperties();
 
             return Ok(labels);
         }
+
+        [HttpGet("labels")]
+        [SwaggerOperation(Summary = "Get all labels",
+          Description = "Requires admin privileges",
+          OperationId = "GetAllCases",
+          Tags = new[] { "Labels" }
+          )]
+        public async Task<IActionResult> GetAllLabels(long identifier)
+        {
+
+            var labels = await this.neo4JDB.GetNodeLabels();
+            return Ok(labels);
+        }
+
+        [HttpGet("{label}")]
+        [SwaggerOperation(Summary = "Get all nodes for a given label",
+           Description = "Requires admin privileges",
+           OperationId = "GetAllCases",
+           Tags = new[] { "Nodes" }
+           )]
+        public async Task<IActionResult> GetNodes(long identifier, string label)
+        {
+            var labels = await neo4JDB.GetNodesWithProperties(label);
+            return Ok(labels);
+        }
+
+        [HttpGet("entities/{entityId}/relations")]
+        [SwaggerOperation(Summary = "Get all relationship of a given entity",
+           Description = "Requires admin privileges",
+           OperationId = "GetAllCases",
+           Tags = new[] { "Nodes" }
+           )]
+        public IActionResult GetEntityRelations(long identifier, long entity_id)
+        {
+            return Ok();
+        }
+        [HttpGet("relations")]
+        [SwaggerOperation(Summary = "Get all relationship of an entity",
+           Description = "Requires admin privileges",
+           OperationId = "GetAllCases",
+           Tags = new[] { "Relations" }
+           )]
+        public IActionResult GetAllRelations(long identifier)
+        {
+            return Ok();
+        }
+
+
+
     }
 }
