eeb6f411ad8351152d8367658cdf587e19cb6b98
Vivek Sarin <vivek@sarins.info>
2023-11-27 16:23:44 -0500

    Initiating Parsers

---
+++ svc/common/info.sarins.services.shared/queues/models/ParserKafkaMessage.cs
@@ -0,0 +1,12 @@
+﻿namespace info.sarins.services.shared.queues.models
+{
+    public class ParserKafkaMessage
+    {
+
+        public string BucketName { get; set; }
+
+        public string FileName { get; set; }
+
+       
+    }
+}
--- svc/core/info.sarins.workers.documents.loader/Worker.cs
+++ svc/core/info.sarins.workers.documents.loader/Worker.cs
@@ -1,5 +1,7 @@
 using Confluent.Kafka;
+using info.sarins.services.shared;
 using info.sarins.services.shared.data;
+using info.sarins.services.shared.queues.models;
 using info.sarins.services.shared.storage.models;
 using Newtonsoft.Json;
 
@@ -9,28 +11,41 @@ namespace info.sarins.workers.documents.loader
     {
         private readonly ILogger<Worker> _logger;
         private readonly IVaultDataService vaultService;
-        private readonly ConsumerConfig kafkaConfig;
-        private readonly string[]? kafkaTopics;
+        private readonly ConsumerConfig kafkaConsumerConfig;
+        private readonly string[]? kafkaConsumerTopics;
+        private readonly ProducerConfig kafkaProducerConfig;
+        private readonly string kafkaProducerTopic;
 
         public Worker(ILogger<Worker> logger, IConfiguration configuration)
         {
             _logger = logger;
+
             this.vaultService = new VaultDataService(default, new VaultDBContext(configuration));
-            kafkaConfig = new ConsumerConfig()
+
+            kafkaConsumerConfig = new ConsumerConfig()
             {
                 BootstrapServers = configuration.GetValue<string>("kafka:hosts"),
                 GroupId = "document-loaders",
                 AutoOffsetReset = AutoOffsetReset.Earliest,
                 EnableAutoCommit = false
             };
-            kafkaTopics = configuration.GetValue<string>("kafka:topics")?.Split(",");
+
+            kafkaProducerConfig = new ProducerConfig
+            {
+                BootstrapServers = configuration.GetValue<string>("kafka:hosts")
+
+            };
+
+            kafkaConsumerTopics = configuration.GetValue<string>("kafka:consumer:topics")?.Split(",");
+            kafkaProducerTopic = configuration.GetValue<string>("kafka:producer:topics") ?? "";
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            using var consumer = new ConsumerBuilder<Ignore, string>(kafkaConfig).Build();
+            using var consumer = new ConsumerBuilder<Ignore, string>(kafkaConsumerConfig).Build();
+            using var producer = new ProducerBuilder<string, string>(kafkaProducerConfig).Build();
 
-            consumer.Subscribe(kafkaTopics);
+            consumer.Subscribe(kafkaConsumerTopics);
             _logger.LogInformation("Starting Kafka Consumer");
             while (!stoppingToken.IsCancellationRequested)
             {
@@ -38,41 +53,60 @@ namespace info.sarins.workers.documents.loader
                 {
                     var consumeResult = consumer.Consume(stoppingToken);
                     var message = consumeResult.Message;
-                    _logger.LogInformation($"Recieved message with key={message.Key}");
                     var eventDetails = JsonConvert.DeserializeObject<MinioEventDetails>(message.Value);
                     /* Update Vault Information
                      * Decision: Should we call Vault or Simply update DB? currently updating DB directly for speed of implementation.                    
                      */
                     foreach (var item in eventDetails?.Records ?? new List<MinioRecord>())
                     {
+                        _logger.LogInformation($"Recieved message for filename '{item.s3.Object.key}' from bucket '{item.s3.Bucket.Name}'.");
                         var vault = await vaultService.GetVaultAsync(item.s3.Bucket.Name);
                         if (vault != null)
                         {
-                            var content = vault.Contents?.Where(c => c.ContentId.Equals(item.s3.Object.key)).First();
+                            if (vault.Contents == null)
+                                vault.Contents = new List<services.shared.http.requests.models.Content>();
+
                             // Currently only ingest added files deleted files should be ignored.
-                            if (content != null)
+                            if (vault.Contents.Any(c => c.FileName.Equals(item.s3.Object.key)))
                             {
-                                content.Stage = services.shared.StageTypes.Uploaded;
+                                var content = vault.Contents?.Where(c => c.FileName.Equals(item.s3.Object.key)).First();
+                                content.Stage = StageTypes.Uploaded;
                                 await vaultService.UpdateVault(vault.VaultId, vault);
                                 _logger.LogInformation($"Vault ({vault.VaultId}) has uploaded content {content.Description}");
                             }
                             else
                             {
-                                if (vault.Contents == null)
-                                    vault.Contents = new List<services.shared.http.requests.models.Content>();
+
                                 vault.Contents.Add(new services.shared.http.requests.models.Content()
                                 {
                                     ContentId = Guid.NewGuid().ToString(),
                                     FileName = item.s3.Object.key,
-                                    Source = services.shared.SourceType.File,
+                                    Source = SourceType.File,
                                     ContentType = item.s3.Object.contentType,
-                                    Stage = services.shared.StageTypes.Uploaded,
+                                    Stage = StageTypes.Uploaded,
 
                                 });
                                 await vaultService.UpdateVault(vault.VaultId, vault);
                                 _logger.LogInformation($"Updated content information for vault");
                             }
                         }
+
+                        // Simply extraction from here on
+                        var producerMessage = new ParserKafkaMessage
+                        {
+                            BucketName = item.s3.Bucket.Name,
+                            FileName = item.s3.Object.key,
+                        };
+
+                        // Sending message for parser to parse file
+                        await producer.ProduceAsync(kafkaProducerTopic, new Message<string, string>
+                        {
+                            Key = producerMessage.BucketName,
+                            Value = producerMessage.ToJson(),
+
+                        });
+
+                        _logger.LogInformation($"Sent message to parsers");
                     }
                     consumer.Commit(consumeResult);
                 }
